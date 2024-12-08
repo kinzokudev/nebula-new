a824bfa844e5fcae531b5c6ce4a349a949009c77
ardishco <ardishco@protonmail.com>
2024-12-08 16:38:15 +0200

    Added stateVersion to home-manager

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,7 +1,10 @@
 {
   nix = {
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       builders-use-substitutes = true;
       # substituters to use
       substituters = [
@@ -11,6 +14,7 @@
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://cosmic.cachix.org/"
+        "https://chaotic-nyx.cachix.org/"
       ];
 
       trusted-public-keys = [
@@ -20,7 +24,12 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
       ];
+      auto-optimise-store = true;
+    };
+    optimise = {
+      automatic = true;
     };
   };
 }
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -5,4 +5,9 @@
     # ./spicetify.nix
     # ./prism.nix
   ];
+  home = {
+    #This option defines the first version of NixOS you have installed on this particular machine, and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+    # You don't need to change this value after updating, It's just gotta match a real NixOS 
+    stateVersion = "24.05";
+  };
 }
